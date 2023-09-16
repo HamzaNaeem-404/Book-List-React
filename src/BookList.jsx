5cf754315969f9b340822d2ed18c1f6d81d82f7f
Hamza Naeem <hamzaquraeshi404@gmail.com>
2023-09-16 21:43:41 +0500

    mapping with json data

---
+++ src/BookList.jsx
@@ -0,0 +1,24 @@
+import Book from './Book'
+import React from 'react'
+import books from './books.json'
+
+
+const BookList = () => {
+  return (
+    <div>
+        {
+            books.map((ele)=>{
+             return <Book
+                image = {ele.image}
+                title = {ele.title}
+                author = {ele.author}
+                price = {ele.price}
+             />
+            })
+        }
+     
+    </div>
+  )
+}
+
+export default BookList
